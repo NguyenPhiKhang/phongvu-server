c3b4000fdbf8939ca214e9267f6f97bbff7fa481
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2021-10-31 18:16:20 +0700

    first repo

---
+++ src/main/java/com/example/phongvuserver/PhongvuServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.phongvuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhongvuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhongvuServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/phongvuserver/PhongvuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phongvuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhongvuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
